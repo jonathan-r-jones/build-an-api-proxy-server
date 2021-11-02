83ed685c1a5029af83b15a785e0cd5bc1aff8919
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2021-11-02 15:04:11 -0400

    Nov-2-2021_3:04_PM

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/api', (req, res) => {
-  res.json({ success: true })
-})
+// Routes
+app.use('/api', require('./routes'))
 
 // Enable cors
 app.use(cors())
