ed46b722693e4997805a376739c88c1c310ca596
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2021-11-03 14:50:07 -0400

    Nov-3-2021_2:50_PM

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+// Rate limitiing
+const limiter = rateLimit( {
+  windowsMs: 10 * 60 * 1000, // 10 minutes
+  max: 10
+})
+app.use(limiter)
+app.set('trust proxy', 1)
+
+// Set static folder
+app.use(express.static('public'))
 
 // Routes
 app.use('/api', require('./routes'))
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@ const url = require('url')
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
+const apicache = require('apicache')
 
 // Env vars
 const API_BASE_URL = process.env.API_BASE_URL
 const API_KEY_NAME = process.env.API_KEY_NAME
 const API_KEY_VALUE = process.env.API_KEY_VALUE
 
-router.get('/', async (req, res) => {
+// Init cache
+let cache = apicache.middleware
+
+router.get('/', cache('2 minutes'), async (req, res) => {
 
   try {
     const params = new URLSearchParams({
@@ -19,6 +23,7 @@ router.get('/', async (req, res) => {
     const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
     const data = apiRes.body
 
+    // Log the request to the public API.
     if(process.env.NODE_ENV !== 'production') {
       console.log(`REQUEST: ${API_BASE_URL}?${params}`);
     }
