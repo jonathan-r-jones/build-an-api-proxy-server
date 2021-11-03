2f869e7487ee4afcf66986b7af909a78227b2746
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2021-11-03 14:26:25 -0400

    Nov-3-2021_2:26_PM

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const url = require('url')
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
@@ -12,6 +13,7 @@ router.get('/', async (req, res) => {
   try {
     const params = new URLSearchParams({
       [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query
     })
 
     const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
